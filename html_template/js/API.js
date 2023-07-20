b38fbd79385c8fd7975d3ddbfb345eaf86601789
NathanPesneau <npesneau@MacBook-Pro-de-Pesneau.local>
2023-07-20 09:57:06 +0200

    ajout ligne de code pour  parcourir database

--- html_template/js/API.js
+++ html_template/js/API.js
@@ -1,10 +1,19 @@
-//Fonction de récupération des data Naoned
-// async function dataEvents() {
-//     const response = await fetch (`https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_agenda-evenements-nantes-nantes-metropole&q=&sort=-date&facet=emetteur&facet=rubrique&facet=lieu&facet=ville&facet=lieu_quartier$`);
-//     const infos = await response.json();
-//     console.log(infos);
-// }
-// dataEvents()
+// Fonction de récupération des data Naoned
+async function dataEvents() {
+    const response = await fetch (`https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_agenda-evenements-nantes-nantes-metropole&q=&sort=-date&facet=emetteur&facet=rubrique&facet=lieu&facet=ville&facet=lieu_quartier`);
+    const infos = await response.json();
+    console.log(infos);
+    // console.log(infos.records[0].fields.emetteur)
+}
+dataEvents()
+
+
+
+
+
+
+
+
 
 //Initialisation de la carte
 let carte = L.map('maCarte').setView([47.218371, -1.553621], 13);
